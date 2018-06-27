95b7f1ef862ddf2920792b8e5b0fffd42a326eb0
Sandra Redo <sredo@advaoptical.com>
2018-06-27 16:26:30 +0200

    Adding basic clases

---
+++ src/Rating.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rating {
+    public enum grades {
+        very_poor, poor, neutral, good, very_good
+    }
+    private Map<String, Integer> Grades = new HashMap<String, Integer>();
+}
