a71d3dd6a82f96db1bdee9a0833539b00a0e6002
Bartek Kowalski <bkowalski1975@gmail.com>
2018-07-12 20:07:19 +0200

    added class AverageRating

---
+++ src/AverageRating.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class AverageRating {
+    public Double avRating(ArrayList x) {
+        Double count = 0.00;
+        for(int i = 0; i < x.size(); i ++) {
+            count += (Double) x.get(i);
+
+        }
+
+    }
+
+}
