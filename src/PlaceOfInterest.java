ae4bdd72c8f01bcf9a15c34d1039ccda9cd2296d
AquaticIntii11 <aquatic.intii@gmail.com>
2018-07-15 12:06:30 +0200

    Adding displaying tourist paths + getters/setters in place of interest

--- src/PlaceOfInterest.java
+++ src/PlaceOfInterest.java
@@ -7,4 +7,52 @@ public class PlaceOfInterest {
     private Rating rating;
     private Location location;
     private float price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/TouristPath.java
+++ src/TouristPath.java
@@ -1,5 +1,7 @@
 import com.sun.xml.internal.ws.api.model.ExceptionType;
 
+import java.util.List;
+
 public class TouristPath {
     private Integer touristPathId; //as primary key
     private String touristPathName; //as primary key
@@ -10,60 +12,26 @@ public class TouristPath {
     private  PlaceOfInterest[] listOfAttractions;
     private String touristPathsJSON;
 
-    //create new path
-    private String createNewTouristPath (String touristPathName){
-        //find last id
-        Integer lastTouristPathId = 0;
-        Integer nextTouristPathId = ++lastTouristPathId;
-        //add path to json file (id+name)
 
-    }
-
-    private boolean checkIfPathNameExists(String touristPathName){
-        //check if exists in touristPathsJSON
-        return false;
-    }
-
-    private get
-
-    public void addPlaceToPath(PlaceOfInterest placeOfInterest, String touristPathName){
-        //check if path exists in json file touristPaths.json
-        //if no, create new path
-
-        if (checkIfPathNameExists()== false ) {
-            createNewTouristPath(touristPatName);
-        };
-        //getTouristPath
-        TouristPath touristPath = getTouristPath(touristPathName);
-        touristPath.addPlace(placeOfInterest);
-    }
-
-    private PlaceOfInterest[] getListOfAttractions(String touristPatName) {
-        //read touristPath from touristPathsJSON
-        //translate to list of attractions
-        return listOfAttractions;
-    }
-
-    public void displayTouristPath(String touristPathName){
+    public void displayTouristPath(List<TouristPath> touristPath){
         try {
-            PlaceOfInterest[] listOfAttractions = getListOfAttractions(touristPathName);
+            System.out.println("Trasy:");
+            for(PlaceOfInterest placeOfInterest: listOfAttractions){
+                System.out.format("Trasa: %s\n", placeOfInterest.getName());
+            }
         } catch (Exception e){
-            System.out.println("Nie istnieje taka ścieżka");
+            System.out.println("Wybrana ścieżka jest pusta");
         }
-        System.out.format("Trasa: %s\n", touristPathName);
-        //placeOfInterests
     }
 
-    public void displayAllTouristPaths(){
+    public void displayAllTouristPaths(List<TouristPath> touristPathsNames){
         try{
-            //read ouristPathsJSON line by line and add to array touristPathsNames[]
-
-
+            for(TouristPath touristPathName: touristPathsNames){
+                System.out.format("%s", touristPathName.touristPathName);
+            }
         } catch (Exception e) {
             System.out.println("Nie zdefiniowano żadnych tras");
         }
-        for(String pathName: touristPathNames){
-            System.out.println(pathName);
-        }
+
     }
 }
