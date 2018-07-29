8d7cda4b260d056687d48a68cf3022a681e113f0
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-07-29 19:19:31 +0200

    First conversion commit.

---
+++ src/main/java/com/infoshareacademy/Configuration.java
@@ -0,0 +1,58 @@
+package com.infoshareacademy;
+
+import java.io.*;
+import java.util.Properties;
+
+public class Configuration {
+
+    private Properties cfg = new Properties();
+
+    public Configuration() {
+        loadConfig();
+    }
+
+    public String getPlacesDB(){
+        return cfg.getProperty("placesDB");
+    }
+
+    public String getDefaultTR(){
+        return cfg.getProperty("defaultTR");
+    }
+
+    public String getUserTR(){
+        return cfg.getProperty("userTR");
+    }
+
+    public String getTripOTD(){
+        return cfg.getProperty("tripOTD");
+    }
+
+    public String getTestFile(){
+        return cfg.getProperty("testFile");
+    }
+
+    private void loadConfig() {
+
+        try {
+            cfg.load(getClass().getClassLoader().getResourceAsStream("cfg.properties"));
+        } catch (FileNotFoundException e) {
+            System.out.println("NIE ZNALEZIONO PLIKU KONFIGURACYJNEGO!");
+        } catch (IOException e) {
+            System.out.println("BŁĄD ODCZYTU!");
+        }
+    }
+
+    public void allLineReader(String FILEPATH){
+
+        String line;
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(FILEPATH))) {
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/infoshareacademy/ReadData.java
@@ -0,0 +1,37 @@
+package com.infoshareacademy;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
+
+public class ReadData {
+
+////  CODE FROM Feature/JZAN-5
+
+    public JSONArray getJSONArray(String FILEPATH) {
+        try {
+            JSONParser jsonParser = new JSONParser();
+            return (JSONArray) jsonParser.parse(new FileReader(FILEPATH));
+        }
+        catch (java.io.IOException | org.json.simple.parser.ParseException exc) {
+            System.out.println("\nBŁĄD ODCZYTU PLIKU: \"" + FILEPATH + "\"!\n");
+        }
+        return null;
+    }
+
+    public JSONArray getSubJSONArray(JSONArray array, int index, String subValue){
+        JSONObject object = (JSONObject) array.get(index);
+        return (JSONArray) object.get(subValue);
+    }
+
+    public JSONObject getJSONObject(JSONArray array, int index){
+        return (JSONObject) array.get(index);
+    }
+
+    public JSONObject getSubJSONObject(JSONArray array, int index, String subValue){
+        JSONArray subArray = getSubJSONArray(array, index, subValue);
+        return (JSONObject) subArray.get(0);
+    }
+
+}
