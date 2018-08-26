56d3f956d42bcab412227c209c4a33c08b629ee4
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-08-26 20:24:17 +0200

    Requested changes

--- src/main/java/com/infoshareacademy/Configuration.java
+++ src/main/java/com/infoshareacademy/Configuration.java
@@ -7,26 +7,31 @@ public class Configuration {
 
     private Properties cfg = new Properties();
 
+    private static final String CFG_FILEPATH = "cfg.properties";
+    private static final String ADMIN_PASS = "adminPass";
+    private static final String PLACES_DB = "placesDB";
+    private static final String DEFAULT_TR = "defaultTR";
+
     public Configuration() {
         loadConfig();
     }
 
     public String getAdminPass() {
-        return cfg.getProperty("adminPass");
+        return cfg.getProperty(ADMIN_PASS);
     }
 
     public String getPlacesDB() {
-        return cfg.getProperty("placesDB");
+        return cfg.getProperty(PLACES_DB);
     }
 
     public String getDefaultTR(){
-        return cfg.getProperty("defaultTR");
+        return cfg.getProperty(DEFAULT_TR);
     }
 
     private void loadConfig() {
 
         try {
-            cfg.load(getClass().getClassLoader().getResourceAsStream("cfg.properties"));
+            cfg.load(getClass().getClassLoader().getResourceAsStream(CFG_FILEPATH));
         } catch (FileNotFoundException e) {
             System.out.println("NIE ZNALEZIONO PLIKU KONFIGURACYJNEGO!");
         } catch (IOException e) {
