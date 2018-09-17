e033e2bcde7fe084673dd7ec7cc9affd2d43f462
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-09-17 20:59:34 +0200

    Cleaning up json places file.

--- web-app/src/main/java/com/infoshareacademy/webapp/model/PlaceConstants.java
+++ web-app/src/main/java/com/infoshareacademy/webapp/model/PlaceConstants.java
@@ -6,21 +6,21 @@ public class PlaceConstants {
 
 //  Json Values
 
-    public static final String PLACE_ID = "ID";
+    public static final String PLACE_ID = "id";
     public static final String PLACE_TYPE = "type";
-    public static final String PLACE_NAME = "Name";
+    public static final String PLACE_NAME = "name";
     public static final String PLACE_DESCRIPTION = "description";
 
-    public static final String PLACE_OPENING_HOURS = "Opening hours: ";
-    public static final String PLACE_PRICES = "Price list";
+    public static final String PLACE_OPENING_HOURS = "openingHours";
+    public static final String PLACE_PRICES = "prices";
     public static final String PLACE_RATINGS = "ratings";
 
-    public static final String PLACE_STREET = "Street";
-    public static final String PLACE_CITY = "City";
-    public static final String PLACE_BUILDING_NUMBER = "Building number";
+    public static final String PLACE_STREET = "street";
+    public static final String PLACE_CITY = "city";
+    public static final String PLACE_BUILDING_NUMBER = "buildingNumber";
     public static final String PLACE_BUILDING_LETTER = "buildingLetter";
     public static final String PLACE_APARTMENT_NUMBER = "apartmentNumber";
-    public static final String PLACE_GPS_COORDINATES = "GPS coordinates";
+    public static final String PLACE_GPS_COORDINATES = "gpsCoordinates";
 
 //  Others
 
