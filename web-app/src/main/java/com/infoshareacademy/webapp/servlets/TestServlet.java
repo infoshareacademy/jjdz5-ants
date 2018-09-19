f2a2321ce4ffa8157640593e1700c7d3e4bae82e
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-09-19 14:58:42 +0200

    Added JS search by name suggestions.

--- web-app/src/main/java/com/infoshareacademy/webapp/servlets/TestServlet.java
+++ web-app/src/main/java/com/infoshareacademy/webapp/servlets/TestServlet.java
@@ -17,8 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet("/places-viewer")
-public class PlacesViewerServlet extends HttpServlet {
+@WebServlet("/tester")
+public class TestServlet extends HttpServlet {
 
     private static final String TEMPLATE_NAME = "places-viewer";
     private static final String PLACES_KEY = "places";
@@ -37,7 +37,6 @@ public class PlacesViewerServlet extends HttpServlet {
         try {
             accessJson.setJsonArray(Configuration.PLACES_JSON_FILEPATH, getServletContext());
             placesRepository.addPlacesToRepository(accessJson.getJsonArray());
-            System.out.println("||PLACES REPOSITORY loaded successfully");
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
@@ -46,11 +45,41 @@ public class PlacesViewerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletConfiguration.setDefaultContentType(resp);
-        Map<String, List> dataModule = new HashMap();
+//        Map<String, List> dataModule = new HashMap();
+        Map<String, String[]> parameterMap = new HashMap();
+        parameterMap = req.getParameterMap();
 
-        dataModule.put(PLACES_KEY, placesRepository.getPlacesRepository());
+        resp.getWriter().println("PARAMETER MAP:<br><br>");
+        for (Map.Entry entry : parameterMap.entrySet()) {
+            resp.getWriter().println("<br><br>parameter: " + entry.getKey() + "<br>");
+            String[] values = (String[]) entry.getValue();
+            resp.getWriter().println("values:<br>");
+            for (String value : values) {
+                resp.getWriter().println(value + "<br>");
+            }
+        }
 
-        templateProvider.print(getServletContext(),TEMPLATE_NAME, dataModule, resp);
+        String value = null;
+
+        if (parameterMap.keySet().contains("placeName")) {
+            String[] values = parameterMap.get("placeName");
+            value = values[0];
+        }
+
+        resp.getWriter().println("<br><br>Your value is: " + value);
+
+        
+
+
+
+//        resp.getWriter().println(
+//                "Parameter map: <br>" +
+//                + "<br><br>Parameter names: <br>" + req.getParameterNames().toString()
+//        );
+
+//        dataModule.put(PLACES_KEY, placesRepository.getPlacesRepository());
+//
+//        templateProvider.print(getServletContext(),TEMPLATE_NAME, dataModule, resp);
     }
 
 //    private Boolean areThereAnyParameters
