e974a295e0a87d94ca41016b3bfd056b4ce24256
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-09-20 11:07:37 +0200

    Search place by name done.

--- web-app/src/main/java/com/infoshareacademy/webapp/servlets/PlacesViewerServlet.java
+++ web-app/src/main/java/com/infoshareacademy/webapp/servlets/PlacesViewerServlet.java
@@ -1,9 +1,9 @@
 package com.infoshareacademy.webapp.servlets;
 
+import com.infoshareacademy.PlaceOfInterestType;
 import com.infoshareacademy.webapp.Configuration;
 import com.infoshareacademy.webapp.freemarker.TemplateProvider;
 import com.infoshareacademy.webapp.mechanics.AccessJson;
-import com.infoshareacademy.webapp.model.Place;
 import com.infoshareacademy.webapp.repository.PlacesRepository;
 import org.json.simple.parser.ParseException;
 
@@ -14,18 +14,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @WebServlet("/places-viewer")
 public class PlacesViewerServlet extends HttpServlet {
 
     private static final String TEMPLATE_NAME = "places-viewer";
     private static final String PLACES_KEY = "places";
+    private static final String PLACES_TYPES_KEY = "placesTypes";
+    private static final String PLACE_NAME_PARAMETER = "pName";
     private static final String PLACE_SELECTION_KEY = "placeSelectionId";
-    private static final String PLACE_NAME_PARAMETER = "placeName";
+    private static final String PLACE_NAME_PARAMETER_KEY = "placeNameParameter";
+    private static final Integer DEFAULT_PLACE_NAME_VALUE = 0;
 
     @Inject
     private TemplateProvider templateProvider;
@@ -35,6 +35,12 @@ public class PlacesViewerServlet extends HttpServlet {
 
     @Inject
     private PlacesRepository placesRepository;
+
+    private Map<String, List> dataModule;
+    private Map<String, String[]> parameters;
+    private List<Integer> placeSelectionId;
+    private List<String> placeNameParameter;
+    private List<PlaceOfInterestType> placesTypes;
 
     @Override
     public void init() throws ServletException {
@@ -50,26 +56,65 @@ public class PlacesViewerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletConfiguration.setDefaultContentType(resp);
-        Map<String, String[]> parameters = req.getParameterMap();
-        Map<String, List> dataModule = new HashMap();
+
+        dataModule = new HashMap();
         dataModule.put(PLACES_KEY, placesRepository.getPlacesRepository());
-        List<Integer> placeSelectionId = new ArrayList<>();
+        fillDataModuleWithPlacesTypes();
 
-        if (ServletParameters.isParameterPresent(parameters, PLACE_NAME_PARAMETER)) {
-            placeSelectionId.add(placesRepository.getPlaceIdByName(getPlaceNameFromParameter(parameters)));
-        } else {
-            placeSelectionId.add(0);
-        }
-
-        dataModule.put(PLACE_SELECTION_KEY, placeSelectionId);
+        parameters = req.getParameterMap();
+        fillDataModuleWithPlaceNameFilter();
 
         templateProvider.print(getServletContext(),TEMPLATE_NAME, dataModule, resp);
     }
 
-    private String getPlaceNameFromParameter(Map<String, String[]> parameters) {
+    private void fillDataModuleWithPlacesTypes() {
+        preparePlacesTypesList();
+        putPlacesTypesToDataModule();
+    }
+
+    private void preparePlacesTypesList() {
+        placesTypes = new ArrayList<>(getAllPlaceTypes());
+    }
+
+    private EnumSet<PlaceOfInterestType> getAllPlaceTypes() {
+        return EnumSet.allOf(PlaceOfInterestType.class);
+    }
+
+    private void putPlacesTypesToDataModule() {
+        dataModule.put(PLACES_TYPES_KEY, placesTypes);
+    }
+
+    private void fillDataModuleWithPlaceNameFilter() {
+        preparePlaceNameLists();
+        if (ServletParameters.isParameterPresent(parameters, PLACE_NAME_PARAMETER)) {
+            addSelectedPlaceToLists(getPlaceNameFromParameter());
+        } else {
+            addDefaultSelectionToList();
+        }
+        putPlaceNameFilterToDataModule();
+    }
+
+    private void preparePlaceNameLists() {
+        placeSelectionId = new ArrayList<>();
+        placeNameParameter = new ArrayList<>();
+    }
+
+    private void addSelectedPlaceToLists(String placeName) {
+        placeSelectionId.add(placesRepository.getPlaceIdByName(placeName));
+        placeNameParameter.add(placeName);
+    }
+
+    private String getPlaceNameFromParameter() {
         return ServletParameters.getOnlyFirstValueOfParameter(parameters.get(PLACE_NAME_PARAMETER));
     }
 
+    private void addDefaultSelectionToList() {
+        placeSelectionId.add(DEFAULT_PLACE_NAME_VALUE);
+    }
 
+    private void putPlaceNameFilterToDataModule() {
+        dataModule.put(PLACE_SELECTION_KEY, placeSelectionId);
+        dataModule.put(PLACE_NAME_PARAMETER_KEY, placeNameParameter);
+    }
 
 }
