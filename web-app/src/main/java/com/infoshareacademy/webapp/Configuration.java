036384176f0eb392cdbe0f35de4366f56d35b59e
Jakub Jurek <jakubjurek.jjd.isa@gmail.com>
2018-09-27 17:34:47 +0200

    Added place promoting mechanics.

--- web-app/src/main/java/com/infoshareacademy/webapp/Configuration.java
+++ web-app/src/main/java/com/infoshareacademy/webapp/Configuration.java
@@ -1,7 +1,14 @@
 package com.infoshareacademy.webapp;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Configuration {
 
     public static final String PLACES_JSON_FILEPATH = "WEB-INF/resources/places.json";
     public static final String ROUTES_JSON_FILEPATH = "WEB-INF/resources/routes.json";
+
+    public static final List<Integer> promotedPlaces = Arrays.asList(3, 5, 6);
+    public static final List<Integer> promotedRoutes = Arrays.asList(1, 2, 3);
+
 }
